3232f7b1ef85e2b01cf773bd1f603bd842810424
Niloufar <nilooooofar97@gmail.com>
2023-12-09 22:11:54 +0100

    add functions

--- script.js
+++ script.js
@@ -1,104 +1,178 @@
-const cardContainer = document.querySelector('.card-container')
+const cardContainer = document.querySelector(".card-container");
 const cards = [
-    {
-        id: 1,
-        name: "bear",
-        img: './assets/images/cards/bear.svg'
-    },
-    {
-        id: 2,
-        name: "bear",
-        img: './assets/images/cards/bear.svg'
-    },
-    {
-        id: 3,
-        name: "crab",
-        img: './assets/images/cards/crab.svg'
-    },
-    {
-        id: 4,
-        name: "crab",
-        img: './assets/images/cards/crab.svg'
-    },
-    {
-        id: 5,
-        name: "shrimp",
-        img: './assets/images/cards/shrimp.svg'
-    },
-    {
-        id: 6,
-        name: "shrimp",
-        img: './assets/images/cards/shrimp.svg'
-    },
-    {
-        id: 7,
-        name: "lion",
-        img: './assets/images/cards/lion.svg'
-    },
-    {
-        id: 8,
-        name: "lion",
-        img: './assets/images/cards/lion.svg'
-    },
-    {
-        id: 9,
-        name: "zebra",
-        img: './assets/images/cards/zebra.svg'
-    },
-    {
-        id: 10,
-        name: "zebra",
-        img: './assets/images/cards/zebra.svg'
-    },
-    {
-        id: 11,
-        name: "fox",
-        img: './assets/images/cards/fox.svg'
-    },
-    {
-        id: 12,
-        name: "fox",
-        img: './assets/images/cards/fox.svg'
-    },
-    {
-        id: 13,
-        name: "jellyfish",
-        img: './assets/images/cards/jellyfish.svg'
-    },
-    {
-        id: 14,
-        name: "jellyfish",
-        img: './assets/images/cards/jellyfish.svg'
-    },
-    {
-        id: 15,
-        name: "penguin",
-        img: './assets/images/cards/penguin.svg'
-    },
-    {
-        id: 16,
-        name: "penguin",
-        img: './assets/images/cards/penguin.svg'
-    },
-]
-cards.map((card) => {
+  {
+    id: 1,
+    name: "bear",
+    img: "./assets/images/cards/bear.svg",
+  },
+  {
+    id: 2,
+    name: "bear",
+    img: "./assets/images/cards/bear.svg",
+  },
+  {
+    id: 3,
+    name: "crab",
+    img: "./assets/images/cards/crab.svg",
+  },
+  {
+    id: 4,
+    name: "crab",
+    img: "./assets/images/cards/crab.svg",
+  },
+  {
+    id: 5,
+    name: "shrimp",
+    img: "./assets/images/cards/shrimp.svg",
+  },
+  {
+    id: 6,
+    name: "shrimp",
+    img: "./assets/images/cards/shrimp.svg",
+  },
+  {
+    id: 7,
+    name: "lion",
+    img: "./assets/images/cards/lion.svg",
+  },
+  {
+    id: 8,
+    name: "lion",
+    img: "./assets/images/cards/lion.svg",
+  },
+  {
+    id: 9,
+    name: "zebra",
+    img: "./assets/images/cards/zebra.svg",
+  },
+  {
+    id: 10,
+    name: "zebra",
+    img: "./assets/images/cards/zebra.svg",
+  },
+  {
+    id: 11,
+    name: "fox",
+    img: "./assets/images/cards/fox.svg",
+  },
+  {
+    id: 12,
+    name: "fox",
+    img: "./assets/images/cards/fox.svg",
+  },
+  {
+    id: 13,
+    name: "jellyfish",
+    img: "./assets/images/cards/jellyfish.svg",
+  },
+  {
+    id: 14,
+    name: "jellyfish",
+    img: "./assets/images/cards/jellyfish.svg",
+  },
+  {
+    id: 15,
+    name: "penguin",
+    img: "./assets/images/cards/penguin.svg",
+  },
+  {
+    id: 16,
+    name: "penguin",
+    img: "./assets/images/cards/penguin.svg",
+  },
+];
+const createCardDiv = (card) => {
     const cardDiv = document.createElement('div');
     cardDiv.classList.add('card');
-    const cardFrontDiv = document.createElement('div');
-    cardFrontDiv.classList.add('card-front');
-    const cardBackDiv = document.createElement('div');
-    cardBackDiv.classList.add('card-back');
-    const cardImg = document.createElement('img');
-    cardImg.src = card.img;
-    cardBackDiv.appendChild(cardImg)
-    cardDiv.appendChild(cardBackDiv)
-    cardDiv.appendChild(cardFrontDiv)
-    cardContainer.appendChild(cardDiv)
-
-})
-const cardsDiv = document.querySelectorAll('.card')
-cardsDiv.forEach((card) => {
-    card.addEventListener('click' , () => {
-        card.classList.toggle('flipped')
+    const frontCardDiv = document.createElement('div');
+    frontCardDiv.classList.add('card-front');
+    const backCardDiv = document.createElement('div');
+    backCardDiv.classList.add('card-back');
+    const backCardImg = document.createElement('img');
+    backCardImg.src = card.img;
+    backCardDiv.appendChild(backCardImg);
+    cardDiv.appendChild(frontCardDiv);
+    cardDiv.appendChild(backCardDiv);
+    return cardDiv;
+}
+cards.map((card) => {
+    const cardDiv = createCardDiv(card);
+    cardContainer.appendChild(cardDiv);
+    cardDiv.addEventListener('click' , () => {
+        flippingCards(cardDiv)
     })
 })
+const flippingCards = (card) =>{
+    card.classList.toggle('flipped')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const createCardDiv = (card) => {
+//   const cardDiv = document.createElement("div");
+//   cardDiv.classList.add("card");
+//   const cardFrontDiv = document.createElement("div");
+//   cardFrontDiv.classList.add("card-front");
+//   const cardBackDiv = document.createElement("div");
+//   cardBackDiv.classList.add("card-back");
+//   const cardImg = document.createElement("img");
+//   cardImg.src = card.img;
+//   cardBackDiv.appendChild(cardImg);
+//   cardDiv.appendChild(cardBackDiv);
+//   cardDiv.appendChild(cardFrontDiv);
+//   cardContainer.appendChild(cardDiv);
+//   return cardDiv;
+// };
+// const flippingCards = (card) => {
+//     card.classList.toggle('flipped')
+// };
+
+// cards.map((card) => {
+//   const cardDiv = createCardDiv(card);
+//   cardContainer.appendChild(cardDiv);
+//   cardDiv.addEventListener('click' , () => {
+//     flippingCards(cardDiv)
+//   })
+// });
+
+
+
+
+
+// cards.map((card) => {
+//     const cardDiv = document.createElement('div');
+//     cardDiv.classList.add('card');
+//     const cardFrontDiv = document.createElement('div');
+//     cardFrontDiv.classList.add('card-front');
+//     const cardBackDiv = document.createElement('div');
+//     cardBackDiv.classList.add('card-back');
+//     const cardImg = document.createElement('img');
+//     cardImg.src = card.img;
+//     cardBackDiv.appendChild(cardImg)
+//     cardDiv.appendChild(cardBackDiv)
+//     cardDiv.appendChild(cardFrontDiv)
+//     cardContainer.appendChild(cardDiv)
+
+// })
+// const cardsDiv = document.querySelectorAll('.card')
+// cardsDiv.forEach((card) => {
+//     card.addEventListener('click' , () => {
+//         card.classList.toggle('flipped')
+//     })
+// })
