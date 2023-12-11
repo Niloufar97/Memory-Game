f31fb0b4b8fa33a852b99412218b0926d36f9065
Niloufar <nilooooofar97@gmail.com>
2023-12-11 10:32:57 +0100

    double and shuffle cards and new game btn

--- script.js
+++ script.js
@@ -1,107 +1,85 @@
 const cardContainer = document.querySelector(".card-container");
+const newGameButton = document.getElementById("new-game-btn");
+
 const cards = [
   {
-    id: 1,
     name: "bear",
     img: "./assets/images/cards/bear.svg",
   },
   {
-    id: 2,
-    name: "bear",
-    img: "./assets/images/cards/bear.svg",
-  },
-  {
-    id: 3,
     name: "crab",
     img: "./assets/images/cards/crab.svg",
   },
   {
-    id: 4,
-    name: "crab",
-    img: "./assets/images/cards/crab.svg",
-  },
-  {
-    id: 5,
     name: "shrimp",
     img: "./assets/images/cards/shrimp.svg",
   },
   {
-    id: 6,
-    name: "shrimp",
-    img: "./assets/images/cards/shrimp.svg",
-  },
-  {
-    id: 7,
     name: "lion",
     img: "./assets/images/cards/lion.svg",
   },
   {
-    id: 8,
-    name: "lion",
-    img: "./assets/images/cards/lion.svg",
-  },
-  {
-    id: 9,
     name: "zebra",
     img: "./assets/images/cards/zebra.svg",
   },
   {
-    id: 10,
-    name: "zebra",
-    img: "./assets/images/cards/zebra.svg",
-  },
-  {
-    id: 11,
     name: "fox",
     img: "./assets/images/cards/fox.svg",
   },
   {
-    id: 12,
-    name: "fox",
-    img: "./assets/images/cards/fox.svg",
-  },
-  {
-    id: 13,
     name: "jellyfish",
     img: "./assets/images/cards/jellyfish.svg",
   },
   {
-    id: 14,
-    name: "jellyfish",
-    img: "./assets/images/cards/jellyfish.svg",
-  },
-  {
-    id: 15,
-    name: "penguin",
-    img: "./assets/images/cards/penguin.svg",
-  },
-  {
-    id: 16,
     name: "penguin",
     img: "./assets/images/cards/penguin.svg",
   },
 ];
+// double and shuffle cards------------------------------------------------------------
+const shufflingCards = () => {
+  let doubleCards = [...cards, ...cards];
+  const doubleCardsWithId = doubleCards.map((card, index) => {
+    return { ...card, id: index + 1 };
+  });
+  const shuffledCards = doubleCardsWithId.sort(() => Math.random() - 0.5);
+  return shuffledCards;
+};
+// create dynamic cards ----------------------------------------------------------------
 const createCardDiv = (card) => {
-    const cardDiv = document.createElement('div');
-    cardDiv.classList.add('card');
-    const frontCardDiv = document.createElement('div');
-    frontCardDiv.classList.add('card-front');
-    const backCardDiv = document.createElement('div');
-    backCardDiv.classList.add('card-back');
-    const backCardImg = document.createElement('img');
-    backCardImg.src = card.img;
-    backCardDiv.appendChild(backCardImg);
-    cardDiv.appendChild(frontCardDiv);
-    cardDiv.appendChild(backCardDiv);
-    return cardDiv;
-}
-cards.map((card) => {
-    const cardDiv = createCardDiv(card);
-    cardContainer.appendChild(cardDiv);
-    cardDiv.addEventListener('click' , () => {
-        flippingCards(cardDiv)
-    })
-})
-const flippingCards = (card) =>{
-    card.classList.toggle('flipped')
-}
+  const cardDiv = document.createElement("div");
+  cardDiv.classList.add("card");
+  const frontCardDiv = document.createElement("div");
+  frontCardDiv.classList.add("card-front");
+  const backCardDiv = document.createElement("div");
+  backCardDiv.classList.add("card-back");
+  const backCardImg = document.createElement("img");
+  backCardImg.src = card.img;
+  backCardDiv.appendChild(backCardImg);
+  cardDiv.appendChild(frontCardDiv);
+  cardDiv.appendChild(backCardDiv);
+  return cardDiv;
+};
+// flippingCards-----------------------------------------------------------------------------
+const flippingCards = (card) => {
+  card.classList.toggle("flipped");
+};
+// rendering cards------------------------------------------------------------------------
+const updateCards = () => {
+  const shuffledcards = shufflingCards();
+  const renderShuffledCards = () => {
+    shuffledcards.map((card) => {
+      const cardDiv = createCardDiv(card);
+      cardContainer.appendChild(cardDiv);
+      cardDiv.addEventListener("click", () => {
+        flippingCards(cardDiv);
+      });
+    });
+  };
+  renderShuffledCards();
+};
+updateCards();
+// new game button fuction------------------------------------------------
+newGameButton.addEventListener("click", () => {
+  cardContainer.innerHTML = "";
+  updateCards();
+});
